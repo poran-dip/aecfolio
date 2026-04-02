2166c8ba20689124522e516d990d1a97cfcf1994
poran-dip <porandip0@gmail.com>
2026-04-02 19:15:31 +0530

    chore: remove auth related changes

--- core/src/lib/auth.ts
+++ core/src/lib/auth.ts
@@ -1,38 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
-import { authConfig } from "@/lib/auth.config";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
   adapter: PrismaAdapter(prisma),
-  callbacks: {
-    async signIn({ user }) {
-      const email = user.email ?? "";
-      // Restrict to @aecian.ac.in institutional emails only
-      if (!email.endsWith("@aecian.ac.in")) {
-        return false;
-      }
-      return true;
-    },
-    async session({ session, user }) {
-      if (session.user && user) {
-        session.user.id = user.id;
-        const dbUser = await prisma.user.findUnique({
-          where: { id: user.id },
-          select: {
-            role: true,
-            student: { select: { id: true } },
-            faculty: { select: { id: true } },
-          },
-        });
-        if (dbUser) {
-          session.user.role = dbUser.role;
-          session.user.studentId = dbUser.student?.id;
-          session.user.facultyId = dbUser.faculty?.id;
-        }
-      }
-      return session;
-    },
-  },
+  providers: [Google],
 });
