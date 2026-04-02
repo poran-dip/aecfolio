fd3683a076521f0fb4fb0504acc5adae5b0b7023
poran-dip <porandip0@gmail.com>
2026-04-02 19:10:24 +0530

    chore: revert next.config.ts - serverExternalPackages handled by Next automatically

--- core/next.config.ts
+++ core/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/client", "@prisma/adapter-pg", "pg"],
+  /* config options here */
 };
 
 export default nextConfig;
