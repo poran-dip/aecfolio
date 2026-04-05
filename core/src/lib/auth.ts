ddd85d8be6442d3b00834fa16f122809966143a2
poran-dip <porandip0@gmail.com>
2026-04-05 14:08:55 +0530

    feat: implement Google OAuth with Auth.js - use Prisma adapter - create auth components and use them in navbar, footer, page - create type definition for user that includes role - move theme toggle into client component

--- core/src/lib/auth.ts
+++ core/src/lib/auth.ts
@@ -6,4 +6,16 @@ import Google from "next-auth/providers/google";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  }
 });
