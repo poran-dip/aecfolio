11fe2c8cb1cf03db39e130fce551ab232a7e0c5b
poran-dip <porandip0@gmail.com>
2026-05-14 22:07:14 +0530

    fix: remove db url missing throw

--- core/src/lib/prisma.ts
+++ core/src/lib/prisma.ts
@@ -2,8 +2,7 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const dbUrl = process.env.DATABASE_URL;
-if (!dbUrl) throw new Error("DATABASE_URL is not set");
+const dbUrl = process.env.DATABASE_URL || "";
 
 const adapter = new PrismaPg({
   connectionString: dbUrl,
