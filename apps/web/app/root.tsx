f7dc6667b6e0a38701b905756984bf23a2eb9797
poran-dip <porandip0@gmail.com>
2026-05-18 22:18:03 +0530

    feat: add shadcn ui primitive components

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,7 +35,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <TooltipProvider>
+          {children}
+          <Toaster />
+        </TooltipProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
