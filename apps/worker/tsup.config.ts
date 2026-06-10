fe3ebb2c3df123954cf0f694d42adf85bbc6a9be
poran-dip <porandip0@gmail.com>
2026-06-10 23:29:54 +0530

    refactor: remove redundant config
    - move common stuff to root
    - remove duplication in build tsconfigs
    - refine tsup configs
    - clean up scripts in package.json files

--- apps/worker/tsup.config.ts
+++ apps/worker/tsup.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  tsconfig: "tsconfig.build.json",
   entry: ["src/index.ts"],
   format: ["esm"],
-  splitting: false,
+  dts: true,
+  sourcemap: true,
   clean: true,
+  splitting: false,
+  minify: true,
+  treeshake: true,
   noExternal: ["@aecfolio/ui", "@aecfolio/shared"],
 });
