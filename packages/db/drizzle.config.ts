f93fde38478fbb11fd4182636e25d5b30c07f2d9
Poran Dip <porandip0@gmail.com>
2026-09-13 11:34:44 +0530

    chore: use dotenv run cli for env loading

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,13 +1,11 @@
-import "dotenv/config";
+import { env } from "@aecfolio/config";
 import { defineConfig } from "drizzle-kit";
-
-const dbUrl = process.env.DATABASE_URL || "";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: dbUrl,
+    url: env.DATABASE_URL,
   },
 });
