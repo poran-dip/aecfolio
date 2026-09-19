13a98cd7b5a5e2c7582b1e17c3acffcd0dc548bb
Poran Dip <porandip0@gmail.com>
2026-09-19 19:10:51 +0530

    chore: remove all db migration files and snapshots

--- packages/db/drizzle/0002_pale_wallop.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "certifications" DROP COLUMN "expiry_date";
--- packages/db/drizzle/0003_illegal_johnny_storm.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "cv_exports" ADD COLUMN "options" jsonb DEFAULT '{}'::jsonb NOT NULL;--> statement-breakpoint
-ALTER TABLE "cv_preferences" ADD COLUMN "options" jsonb DEFAULT '{}'::jsonb NOT NULL;
